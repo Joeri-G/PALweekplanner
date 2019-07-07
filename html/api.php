0f89aca589f703258cdbd72ae667580031015e1f
Joeri "Me" Geuzinge <3677joeri@gmail.com>
2019-07-07 14:56:36 +0200

    Added first submit checks and databsepush. Teacher work days next up.

--- html/api.php
+++ html/api.php
@@ -1,10 +1,22 @@
 <?php
-if (isset($_GET['getList'])) {
-  //get data from config file
+//make sure user is loggedin
+session_start();
+if (!isset($_SESSION['loggedin'])) {
+  header("location: /login");
+  die();
+}
+//generate options list, alsom make sure user has permission
+if (isset($_GET['getList']) && $_SESSION['userLVL'] >= 2) {
   header('Content-Type: application/json');
-  //decode the JSON to object
   die('{"klas": ["4v1", "4v2", "4v3"], "docent": ["Gast", "Bos", "Van den Berg"]}');
 }
 
+//include submit script when user selects submit, also make sure user has permission
+if (isset($_GET['submit']) && $_GET['submit'] == true && $_SESSION['userLVL'] >= 2) {
+  require('../php/submit.php');
+  die();
+}
+
+//just dump the _GET as a test or something
 print_r($_GET);
  ?>
--- html/login/index.php
+++ html/login/index.php
@@ -1,8 +1,16 @@
 <?php
+//tmp login
+// <tmp>
 session_start();
 if (!isset($_SESSION['loggedin'])) {
   $_SESSION['loggedin'] = true;
   $_SESSION['username'] = 'testuser';
+  //user 'level' (user, admin etc)
+  $_SESSION['userLVL'] = 3;
 }
 header("location: /");
+die();
+// </tmp>
+
+//actual databse connection, hashes, salts, peppers etc. go here
  ?>
